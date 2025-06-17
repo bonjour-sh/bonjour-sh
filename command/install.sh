5dde103b00a8b930a3fb035010c2107682177e52
mehov <mehov@users.noreply.github.com>
2025-06-17 10:23:09 +0000

    Clarify prompting in command/install.sh
    - Shorten and/or clarify code comments
    
    - Move sourcing .env files to the preparation loop
      A future plan is storing prompt answers from all installers into
      \$HOME/.bonjour.env. On repeated runs, those values should have
      priority over the defaults. To be able to overwrite the installer
      defaults, sourcing \$HOME/.bonjour.env should happen after sourcing
      the individual installer .env files. Therefore code had to be moved.

--- command/install.sh
+++ command/install.sh
@@ -25,21 +25,27 @@ done
 
 _install_command() (
     _installers=$(_input 'installers' 'Choose what to install' "$_available_installers" "$@")
-    # 0. Load all selected installers
+    # 0. Load all selected installers and their default configurations (if any)
     for _installer_name in $_installers; do
         # Source both general and OS-specific scripts for this installer
         if [ -f "${_installer_path}/${_installer_name}/${_installer_name}.${BONJOUR_OS}.sh" ]; then
             . "${_installer_path}/${_installer_name}/${_installer_name}.${BONJOUR_OS}.sh"
         fi
+        # Source the installer itself
         . "${_installer_path}/${_installer_name}/${_installer_name}.sh"
-    done
-    # 1. (Interactive) Collect input required for all selected installers
-    for _installer_name in $_installers; do
-        # If installer provides .env file, get configuration variables ready
+        # Shorthand to what would be this installer's .env path
         _installer_env="${_installer_path}/${_installer_name}/.${_installer_name}.env"
         if [ -f "$_installer_env" ]; then
-            # Source the installer .env file to have default values
+            # Source the installer .env file to have default values ready
             . "$_installer_env"
+        fi
+    done
+    # 1. (Interactive) Collect input required for selected installers
+    for _installer_name in $_installers; do
+        # Shorthand to what would be current installer's .env path
+        _installer_env="${_installer_path}/${_installer_name}/.${_installer_name}.env"
+        # If exists, it contains prompts (with help and defaults) for current installer
+        if [ -f "$_installer_env" ]; then
             # Start with empty help text, will append if needed as we loop below
             _env_help=''
             # Go through each line in .env, collecting variables and metadata
@@ -52,13 +58,13 @@ _install_command() (
                     [a-zA-Z_]*=*)
                         # Line starts with valid shell variable name
                         _env_key=$(echo "$line" | sed -n 's/^\([a-zA-Z_][a-zA-Z0-9_]*\)=.*$/\1/p')
-                        # Everything after # on that line is a prompt text
+                        # Everything after # on that line is prompt text
                         _env_prompt=$(echo "$line" | sed -n 's/.*# *\(.*\)/\1/p')
                         # If no prompt text on that line, default to 'provide X'
                         if [ -z "$_env_prompt" ]; then
                             _env_prompt="Provide ${_env_key}"
                         fi
-                        # Use value from .env file to suggest as default answer
+                        # Read value from environment to suggest default answer
                         eval "_env_default=\${$_env_key}"
                         # Collect value from callback (if any), else call _input
                         _func="_${_installer_name}_prompt_${_env_key}"
@@ -69,7 +75,7 @@ _install_command() (
                             # Call _input for this variable
                             _env_value=$(_input "$_env_key" "$_env_prompt" "$_env_default" "$_env_help" "$@")
                         fi
-                        # Overwrite the variable with the value we collected
+                        # Overwrite environment variable with value we collected
                         eval "${_env_key}=\${_env_value}"
                         # Clean up
                         unset -v _env_key _env_default _func _env_value
