0afd771e7d652d8b79c5cb73bd7e8768aa2f20f7
mehov <mehov@users.noreply.github.com>
2025-07-16 14:52:00 +0000

    Add bonjour host home

--- command/host.sh
+++ command/host.sh
@@ -21,6 +21,10 @@ _host_command_list() (
     done
 )
 
+_host_command_home() (
+    printf '%s' "$(_ www_root)/${1}"
+)
+
 _host_command_add() {
     _domain=$(_input 'domain' 'Domain name for the new web service' '' '' "$@")
     if [ -z "$_domain" ]; then
@@ -31,17 +35,17 @@ _host_command_add() {
     if [ "$BONJOUR_NONINTERACTIVE" != "true" ] && [ -n "$_aliases" ]; then
         _prompt_alias
     fi
-    _web_root="$(_ www_root)/${_domain}"
+    _web_home="$(_host_command_home ${_domain})"
     cat > "$(_ local_etc)/nginx/conf.d/vhost_${_domain}_80.conf" <<-EOF
 	server {
 	    server_name ${_domain} ${_aliases};
 	    listen 80;
-	    include ${_web_root}/nginx*.conf;
+	    include ${_web_home}/nginx*.conf;
 	    include snippets/vhost.conf;
 	}
 	EOF
-    mkdir -p "${_web_root}"
-    mkdir -p "${_web_root}/public"
+    mkdir -p "${_web_home}"
+    mkdir -p "${_web_home}/public"
     service nginx restart
     _certbot_certonly "${_domain} ${_aliases}"
     if [ $? -ne 0 ]; then
@@ -49,17 +53,17 @@ _host_command_add() {
         rm "$(_ local_etc)/nginx/conf.d/vhost_${_domain}_80.conf"
         return 1
     fi
-    mkdir -p "${_web_root}/ssl"
-    ln -s "$(_ local_etc)/letsencrypt/live/${_domain}/fullchain.pem" "${_web_root}/ssl/cert"
-    ln -s "$(_ local_etc)/letsencrypt/live/${_domain}/privkey.pem" "${_web_root}/ssl/key"
+    mkdir -p "${_web_home}/ssl"
+    ln -s "$(_ local_etc)/letsencrypt/live/${_domain}/fullchain.pem" "${_web_home}/ssl/cert"
+    ln -s "$(_ local_etc)/letsencrypt/live/${_domain}/privkey.pem" "${_web_home}/ssl/key"
     cat > "$(_ local_etc)/nginx/conf.d/vhost_${_domain}_443.conf" <<-EOF
 	server {
 	    server_name ${_domain} ${_aliases};
 	    listen 443 ssl;
 	    # No variables means Nginx preloads certs on start with root privileges
-	    ssl_certificate ${_web_root}/ssl/cert;
-	    ssl_certificate_key ${_web_root}/ssl/key;
-	    include ${_web_root}/nginx*.conf;
+	    ssl_certificate ${_web_home}/ssl/cert;
+	    ssl_certificate_key ${_web_home}/ssl/key;
+	    include ${_web_home}/nginx*.conf;
 	    include snippets/vhost.conf;
 	    include snippets/vhost_ssl.conf;
 	}
