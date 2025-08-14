71b56e4de14283e84dcd00e0d6bc550403aa91c3
mehov <mehov@users.noreply.github.com>
2025-08-14 16:20:38 +0000

    Disable bind-address

--- command/install-scripts/mariadb/mariadb.sh
+++ command/install-scripts/mariadb/mariadb.sh
@@ -2,12 +2,14 @@
 #
 # MariaDB installer
 
+_local_etc=$(_ local_etc)
+
 mariadb_root_password_default=$(_random_string 24)
 
 _mariadb_install() (
     _package install mariadb-server
     mysqladmin -u root password "${mariadb_root_password}"
-    _config "$(_ local_etc)/mysql/my.cnf" '#' '=' 'port' "${mariadb_port}"
+    _config "${_local_etc}/mysql/my.cnf" '#' '=' 'port' "${mariadb_port}"
     if [ -n "$whitelisted_hosts" ]; then
         _firewall 'mariadb-restrict' flush
         for _whitelisted_host in $whitelisted_hosts; do
@@ -17,6 +19,9 @@ _mariadb_install() (
         _firewall 'mariadb-restrict' deny '' in ":${mariadb_port}"
         _firewall 'mariadb-restrict' deny ":${mariadb_port}" out ''
     fi
+    # Disable specific bind-address allows all interfaces thus enables remote connections
+    _server_cnf=$(grep -liFR "bind-address" --include="*server.cnf" "${_local_etc}/mysql")
+    _config "$_server_cnf" '#' '=' 'bind-address'
 )
 
 _mariadb_post_install_debian() (
